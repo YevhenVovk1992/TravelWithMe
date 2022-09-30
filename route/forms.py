24d51b7ac4f8597da164757567fb4f9652333cfe
Vovk <vovk.valki@ukr.net>
2022-09-30 12:59:01 +0300

    Make models and forms
    add form EventForm;
    add form RouteForm;
    make migration;
    migrate;

---
+++ route/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from route import models
+
+
+class EventForm(forms.ModelForm):
+
+    id_route = forms.ModelMultipleChoiceField(queryset=models.Route.objects.all())
+    event_admin = forms.IntegerField(label='event_admin')
+    start_date = forms.DateField()
+    price = forms.IntegerField(label='Price')
+
+    class Meta:
+        model = models.Event
+        fields = ('id_route', 'event_admin', 'start_date', 'price')
+
+
+class RouteForm(forms.ModelForm):
+
+    start_point = forms.IntegerField(label='start place')
+    destination = forms.IntegerField(label='finish place')
+    route_type = forms.ChoiceField()
+    country = forms.CharField(max_length=50, label='country')
+    location = forms.CharField(max_length=50, label='location')
+    description = forms.CharField(widget=forms.Textarea)
+    duration = forms.IntegerField()
+
+    class Meta:
+        model = models.Event
+        fields = ('start_point', 'destination', 'route_type', 'country', 'location', 'description', 'duration')
